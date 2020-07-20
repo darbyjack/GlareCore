c339dbca084c7523e7ec454011fac5fdd059758d
darbyjack <admin@glaremasters.me>
2020-07-19 22:45:02 -0500

    Implemented builder for core

---
+++ src/main/java/me/glaremasters/glarecore/GlareCore.java
@@ -0,0 +1,71 @@
+package me.glaremasters.glarecore;
+
+import ch.jalu.configme.SettingsManager;
+import ch.jalu.configme.SettingsManagerBuilder;
+import ch.jalu.configme.configurationdata.ConfigurationData;
+import co.aikar.commands.PaperCommandManager;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class GlareCore {
+    private final BukkitAudiences bukkitAudiences;
+    private final MiniMessage miniMessage;
+    private final PaperCommandManager commandManager;
+    private final SettingsManager settingsManager;
+
+    public BukkitAudiences getBukkitAudiences() {
+        return bukkitAudiences;
+    }
+
+    public MiniMessage getMiniMessage() {
+        return miniMessage;
+    }
+
+    public PaperCommandManager getCommandManager() {
+        return commandManager;
+    }
+
+    public SettingsManager getSettingsManager() {
+        return settingsManager;
+    }
+
+    private GlareCore(GlareCoreBuilder builder) {
+        this.bukkitAudiences = builder.bukkitAudiences;
+        this.miniMessage = builder.miniMessage;
+        this.commandManager = builder.commandManager;
+        this.settingsManager = builder.settingsManager;
+    }
+
+    public static class GlareCoreBuilder {
+        private BukkitAudiences bukkitAudiences;
+        private MiniMessage miniMessage;
+        private final PaperCommandManager commandManager;
+        private SettingsManager settingsManager;
+
+        public GlareCoreBuilder(final PaperCommandManager commandManager) {
+            this.commandManager = commandManager;
+        }
+
+        public GlareCoreBuilder useAdventure(final JavaPlugin plugin) {
+            this.bukkitAudiences = BukkitAudiences.create(plugin);
+            return this;
+        }
+
+        public GlareCoreBuilder useMiniMessage() {
+            this.miniMessage = MiniMessage.get();
+            return this;
+        }
+
+        public GlareCoreBuilder useConfigMe(final File file, final ConfigurationData configurationData) {
+            this.settingsManager = SettingsManagerBuilder.withYamlFile(file).useDefaultMigrationService().configurationData(configurationData).create();
+            return this;
+        }
+
+        public GlareCore build() {
+            return new GlareCore(this);
+        }
+    }
+}
