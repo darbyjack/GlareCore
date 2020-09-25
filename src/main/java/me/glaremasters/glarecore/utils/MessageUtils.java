571dea6c9ccf894b7388702a2900a5a53b2ee59c
darbyjack <admin@glaremasters.me>
2020-09-24 19:54:52 -0500

    Updated & Pushed to 1.2.0.

--- src/main/java/me/glaremasters/glarecore/utils/MessageUtils.java
+++ src/main/java/me/glaremasters/glarecore/utils/MessageUtils.java
@@ -1,7 +1,9 @@
 package me.glaremasters.glarecore.utils;
 
 import co.aikar.commands.CommandIssuer;
+import co.aikar.commands.PaperCommandManager;
 import co.aikar.locales.MessageKeyProvider;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public class MessageUtils {
@@ -15,5 +17,16 @@ public class MessageUtils {
      */
     public static String asString(@NotNull final CommandIssuer issuer, @NotNull final MessageKeyProvider key) {
         return issuer.getManager().getLocales().getMessage(issuer, key);
+    }
+
+    /**
+     * Helper method to convert a message key from lang files into a String
+     *
+     * @param manager the manager of the plugin
+     * @param key     the key to translate
+     * @return key translated to string
+     */
+    public static String asString(@NotNull final PaperCommandManager manager, @NotNull final MessageKeyProvider key) {
+        return manager.getLocales().getMessage(manager.getCommandIssuer(Bukkit.getConsoleSender()), key);
     }
 }
